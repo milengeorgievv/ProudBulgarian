ff211f09f879beba440f3302278bae568ca4094d
Milen Georgiev <milegeorgiev@tu-sofia.bg>
2021-03-25 23:21:05 +0200

    Add project files.

---
+++ ProudBulgarian/ProudBulgarian/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProudBulgarian
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Profile_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Singleplayer_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Multiplayer_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Challenge_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Friends_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rankings_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
